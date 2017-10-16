426fc665216db6b2134296883731414797e82464
lucasdelatorre <luksdlt92@gmail.com>
2017-10-15 21:09:28 -0300

    Improving the chat integration

--- src/containers/Admin/Chats/conversation.js
+++ src/containers/Admin/Chats/conversation.js
@@ -1,9 +1,16 @@
-import React, { Component } from 'react';
-import { Button, Input } from 'antd';
-import { messages } from './fakeData';
+import React from 'react';
+import { Button, Input, Row } from 'antd';
 import Cable from 'actioncable';
 
-const { TextArea } = Input.TextArea
+const ulStyle = {
+  'border': '1px solid #ccc',
+  'height': '300px',
+  'margin': '0',
+  'padding': '10px',
+  'margin-bottom': '10px',
+  'border-radius': '3px',
+  'list-style-type': 'none'
+}
 
 export default class Conversation extends React.Component {
 
@@ -12,41 +19,53 @@ export default class Conversation extends React.Component {
     this.state = {
       conversationId: props.match.params.conversationId,
       currentChatMessage: '',
-      chatLogs: messages
+      chatLogs: []
     };
   }
 
   componentWillMount() {
-    this.createSocket();
+    this.createSocket()
+  }
+
+  componentWillUnmount() {
+    this.cable.disconnect()
   }
 
   createSocket() {
     let cable = Cable.createConsumer('ws://localhost:3000/cable/');
+    this.cable = cable
     this.chats = cable.subscriptions.create({
     	user_token: localStorage.getItem('auth_token'),
       channel: 'ChatChannel',
       conversation_id: this.state.conversationId
     }, {
       connected: () => {
-      	console.log('Connected')
+        this.chats.historial()
       },
-      received: (data) => {
-        //let chatLogs = this.state.chatLogs;
-        //chatLogs.push(data);
-        //this.setState({ chatLogs: chatLogs });
-        console.log(data)
+      received: (stream) => {
+        if (stream.type === 'historial') {
+          console.log('Historial')
+          let chatLogs = this.state.chatLogs
+          this.setState({ chatLogs: chatLogs.concat(stream.data) })
+        } else if (stream.type === 'new_message') {
+          let chatLogs = this.state.chatLogs
+          chatLogs.push(stream.data)
+          this.setState({ chatLogs: chatLogs })
+        }
       },
       create: function(chatContent) {
         this.perform('create', {
           content: chatContent
-        });
+        })
+      },
+      historial: function() {
+        this.perform('historial')
       }
     });
   }
 
   renderChatLog() {
     return this.state.chatLogs.map((msg) => {
-    	console.log('Hola ' + msg.content)
       return (
         <li key={`chat_${msg.id}`}>
           <span className='chat-message'>{ msg.content }</span>
@@ -55,15 +74,39 @@ export default class Conversation extends React.Component {
     });
   }
 
+  updateCurrentChatMessage(event) {
+    this.setState({
+      currentChatMessage: event.target.value
+    });
+  }
+
+  handleSendEvent(event) {
+    event.preventDefault();
+    this.chats.create(this.state.currentChatMessage);
+    this.setState({ currentChatMessage: '' });
+  }
+
 	render () {
 		return (
-			<div>
-				<ul>
-        	{ this.renderChatLog() }
-        </ul>
-        <Input />
-        <Button />
-			</div>
+      <div className="isoLayoutContentWrapper">
+        <div className="isoLayoutContent">
+  				<ul style={ ulStyle }>
+          	{ this.renderChatLog() }
+          </ul>
+          <Row type="flex" justify="start">
+            <Input 
+              style={{ width: '80%', marginRight: '10px' }}
+              value={this.state.currentChatMessage}
+              onChange={(e) => this.updateCurrentChatMessage(e)}
+            />
+            <Button 
+              type="primary" 
+              onClick={(e) => this.handleSendEvent(e)}>
+              Enviar
+            </Button>
+          </Row>
+        </div>
+      </div>
 		)
 	}
 }
